4867713bd00d2c4bfb2b75323b2f069fa2163451
Alek Pinchuk <alek@nexenta.com>
2016-09-01 08:49:41 -0700

    NEX-7543 backout async delete (NEX-5085 and NEX-6151) Reviewed by: Roman Strashkin <roman.strashkin@nexenta.com> Reviewed by: Yuri Pankov <yuri.pankov@nexenta.com> Reviewed by: Sanjay Nadkarni <sanjay.nadkarni@nexenta.com>

--- usr/src/uts/common/fs/zfs/zfs_vnops.c
+++ usr/src/uts/common/fs/zfs/zfs_vnops.c
@@ -22,8 +22,10 @@
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Portions Copyright 2007 Jeremy Teo
  * Portions Copyright 2010 Robert Milkowski
- * Copyright (c) 2013, 2014 by Delphix. All rights reserved.
- * Copyright 2014 Nexenta Systems, Inc.  All rights reserved.
+ * Copyright (c) 2012, 2015 by Delphix. All rights reserved.
+ * Copyright (c) 2014 Integros [integros.com]
+ * Copyright 2015 Joyent, Inc.
+ * Copyright 2016 Nexenta Systems, Inc.  All rights reserved.
  */
 
 #include <sys/types.h>
@@ -4430,7 +4432,7 @@ out:
 
 /*ARGSUSED*/
 static void
-zfs_inactive_impl(vnode_t *vp, cred_t *cr, caller_context_t *ct)
+zfs_inactive(vnode_t *vp, cred_t *cr, caller_context_t *ct)
 {
 	znode_t	*zp = VTOZ(vp);
 	zfsvfs_t *zfsvfs = zp->z_zfsvfs;
@@ -4487,46 +4489,6 @@ zfs_inactive_impl(vnode_t *vp, cred_t *cr, caller_context_t *ct)
 
 	zfs_zinactive(zp);
 	rw_exit(&zfsvfs->z_teardown_inactive_lock);
-}
-
-static void
-zfs_inactive_task(void *task_arg)
-{
-	vnode_t *vp = (vnode_t *)task_arg;
-	ASSERT(vp);
-	zfs_inactive_impl(vp, CRED(), NULL);
-}
-
-/*
- * This value will be multiplied by zfs_dirty_data_max to determine
- * the threshold past which we will call zfs_inactive_impl() async.
- *
- * Selecting the multiplier is a balance between how long we're willing to wait
- * for delete/free to complete (get shell back, have a NFS thread captive, etc)
- * and reducing the number of active requests in the backing taskq.
- *
- * 4 GiB (zfs_dirty_data_max default) * 16 (multiplier default) = 64 GiB
- * meaning by default we will call zfs_inactive_impl async for vnodes > 64 GiB
- */
-uint16_t zfs_inactive_async_multiplier = 16;
-
-void
-zfs_inactive(vnode_t *vp, cred_t *cr, caller_context_t *ct)
-{
-	znode_t	*zp = VTOZ(vp);
-
-	if (zp->z_size > zfs_inactive_async_multiplier * zfs_dirty_data_max) {
-		if (taskq_dispatch(dsl_pool_vnrele_taskq(
-		    dmu_objset_pool(zp->z_zfsvfs->z_os)), zfs_inactive_task,
-		    vp, TQ_SLEEP) != NULL)
-			return; /* task dispatched, we're done */
-	}
-
-	/*
-	 * If the size of the vnode is <= than the threshold computed above
-	 * or if the taskq dispatch failed - do a sync zfs_inactive call
-	 */
-	zfs_inactive_impl(vp, cr, ct);
 }
 
 /*
