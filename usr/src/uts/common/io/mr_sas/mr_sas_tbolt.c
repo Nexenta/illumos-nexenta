6668c4d9e72e2ebf8b46706843f147c0df8ded63
Joyce McIntosh <joyce.mcintosh@nexenta.com>
2018-07-25 17:37:18 -0600

    NEX-17502 Slow crash dumps, significantly slower than live core Reviewed by: Dan Fields <dan.fields@nexenta.com> Reviewed by: Yuri Pankov <yuri.pankov@nexenta.com> Reviewed by: Rick McNeal <rick.mcneal@nexenta.com> Reviewed by: Sanjay Nadkarni <sanjay.nadkarni@nexenta.com>
    Conflicts:
    
    	usr/src/uts/common/io/mr_sas/mr_sas_tbolt.c

--- usr/src/uts/common/io/mr_sas/mr_sas_tbolt.c
+++ usr/src/uts/common/io/mr_sas/mr_sas_tbolt.c
@@ -16,7 +16,7 @@
  */
 
 /*
- * Copyright 2013 Nexenta Systems, Inc.  All rights reserved.
+ * Copyright 2018 Nexenta Systems, Inc.  All rights reserved.
  */
 
 
@@ -1096,7 +1096,7 @@ wait_for_outstanding_poll_io(struct mrsas_instance *instance)
 		if (instance->fw_outstanding <= 2) {
 			break;
 		}
-		drv_usecwait(10*MILLISEC);
+		drv_usecwait(MILLISEC);
 		/* complete commands from reply queue */
 		(void) mr_sas_tbolt_process_outstanding_cmd(instance);
 	}
